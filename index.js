ddb2296183617f97ae8562afa3b0e69759b0b7b6
Mircea Cmg <mirceacmg@192-168-0-103.rdsnet.ro>
2020-10-12 13:56:27 +0300

    hide private information

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://mirceaCmg:Anacapatina12@cluster0.8yvav.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.json({ "hello ~": "Hi ~~" });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      userData: doc,
+    });
+  });
 });
 
 app.listen(5000);
